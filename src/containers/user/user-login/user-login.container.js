b2d8470f181c254f2c30dad3a5715b1a3423adeb
rikijenifer <rikijenifer15@gmail.com>
2021-03-30 10:38:46 +0700

    Google Signin New

--- src/containers/user/user-login/user-login.container.js
+++ src/containers/user/user-login/user-login.container.js
@@ -63,7 +63,7 @@ const Login = (props) => {
   useEffect(() => {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId: '324403427572-p0be2b4r7ga5mnm3v2jg9kd9rs2ei5if.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      webClientId: '534998452385-ipfkf6enogkk6j3c69dj35qjf2q9n29l.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
       // hostedDomain: '', // specifies a hosted domain restriction
       // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
