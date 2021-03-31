85a6e0c7f03231728ee86f2aca55e141ad534f36
rikijenifer <rikijenifer15@gmail.com>
2021-03-31 14:40:33 +0700

    Halaman Change Password

--- src/containers/user/user-verify/user-verify-password.container.js
+++ src/containers/user/user-verify/user-verify-password.container.js
@@ -2,13 +2,14 @@ import * as Yup from 'yup'
 import PropTypes from 'prop-types'
 import { useFormik } from 'formik'
 import React, { useState } from 'react'
-import { Text } from '@ui-kitten/components'
+import { Text,Icon, } from '@ui-kitten/components'
 import { useNavigation } from '@react-navigation/native'
 
 import { 
     View, 
     ScrollView,
     TouchableOpacity,
+    TouchableWithoutFeedback,
     } 
 from 'react-native'
 import { 
@@ -21,21 +22,24 @@ import { styles } from './user-verify.style'
 
 const UserVerifyPassword = () => {
   const navigation = useNavigation()
-
+  
   const [loading, setLoading] = useState(false)
   const [success] = useState(true)
-
+  const [secureTextEntry, setSecureTextEntry] = useState(true)
+  const [secureTextEntryNewPassword, setSecureTextEntryNewPassword] = useState(true)
+  const [secureTextEntryConfirmPassword, setSecureTextEntryConfirmPassword] = useState(true)
   const FormSubmit = useFormik({
-    initialValues: { verify_code: '' },
+    initialValues: { old_password: '', new_password: '', confirm_password: '' },
     validationSchema: Yup.object({
-      verify_code: Yup.string()
-        .required('Kode verifikasi harus diisi'),
+      old_password: Yup.string().required('Password lama harus diisi'),
+      new_password: Yup.string().required('Password baru harus diisi'),
+      confirm_password: Yup.string().required('Konfirmasi password harus diisi'),
     }),
     onSubmit: () => {
       setLoading(true)
       try {
         if (success === true) {
-          navigation.navigate('ConfirmPassword')
+          navigation.navigate('Profil')
         }
       } catch (err) {
         return err
@@ -44,28 +48,68 @@ const UserVerifyPassword = () => {
     },
   })
 
+  const toggleSecureEntry = () => {
+    setSecureTextEntry(!secureTextEntry)
+  }
+
+  const toggleSecureEntryNewPassword = () => {
+    setSecureTextEntryNewPassword(!secureTextEntryNewPassword)
+  }
+
+  const toggleSecureEntryConfirmPassword = () => {
+    setSecureTextEntryConfirmPassword(!secureTextEntryConfirmPassword)
+  }
+
+  const renderIcon = props => (
+    <TouchableWithoutFeedback onPress={toggleSecureEntry}>
+      <Icon {...props} name={secureTextEntry ? 'eye-off' : 'eye'} />
+    </TouchableWithoutFeedback>
+  )
+
+  const renderIconNewPassword = props => (
+    <TouchableWithoutFeedback onPress={toggleSecureEntryNewPassword}>
+      <Icon {...props} name={secureTextEntryNewPassword ? 'eye-off' : 'eye'} />
+    </TouchableWithoutFeedback>
+  )
+
+  const renderIconConfirmPassword = props => (
+    <TouchableWithoutFeedback onPress={toggleSecureEntryConfirmPassword}>
+      <Icon {...props} name={secureTextEntryConfirmPassword ? 'eye-off' : 'eye'} />
+    </TouchableWithoutFeedback>
+  )
+
   return (
     <>
       {loading && <Loader loading={loading} setLoading={setLoading} />}
-      <Topbar title='Verifikasi Akun' backIcon={true} />
+      <Topbar title='Ubah Password' backIcon={true} />
       <View style={styles.container}>
         <ScrollView showsVerticalScrollIndicator={false}>
           <View style={{ marginTop: 30 }}>
-            <Text style={styles.content}>
-            Kode verifikasi akun dikirim lewat email pengguna.
-            Masukkan 8 digit kode di bawah untuk dapat melakukan verifikasi.
-            </Text>
-            <Text style={styles.text}>Masukkan kode verifikasi</Text>
+            <Text style={styles.text}>Masukkan password lama</Text>
             <TextBox
-              name='verify_code'
+              name='old_password'
               form={FormSubmit}
-              placeholder='Kode Verifikasi'
+              placeholder='Password Lama'
+              accessoryRight={renderIcon}
+              secureTextEntry={secureTextEntry}
             />
-            <TouchableOpacity
-              onPress={() => console.log("reset")}>
-              <Text style={styles.leftText}>Reset kode verifikasi</Text>
-            </TouchableOpacity>
-            <Buttons title='Verifikasi'
+            <Text style={styles.text}>Masukkan password baru</Text>
+            <TextBox
+              name='new_password'
+              form={FormSubmit}
+              placeholder='Password Baru'
+              accessoryRight={renderIconNewPassword}
+              secureTextEntryNewPassword={secureTextEntryNewPassword}
+            />
+            <Text style={styles.text}>Konfirmasi password baru</Text>
+            <TextBox
+              name='confirm_password'
+              form={FormSubmit}
+              placeholder='Konfirmasi Password Baru'
+              accessoryRight={renderIconConfirmPassword}
+              secureTextEntryConfirmPassword={secureTextEntryConfirmPassword}
+            />
+            <Buttons title='Ubah Password'
               onPress={FormSubmit.handleSubmit}/>
           </View>
         </ScrollView>
