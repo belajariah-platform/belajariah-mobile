633a78dfb34b3e0b7a5204d410634a7e640f3a43
rikijenifer <rikijenifer15@gmail.com>
2021-11-26 22:09:30 +0700

    Hide Scope Google Signin

--- src/containers/user/user-login/user-login.container.js
+++ src/containers/user/user-login/user-login.container.js
@@ -83,7 +83,7 @@ const Login = (props) => {
     GoogleSignin.configure({
       offlineAccess: true,
       forceCodeForRefreshToken: true,
-      scopes: [`${Config.GOOGLE_SCOPES}`],
+      // scopes: [`${Config.GOOGLE_SCOPES}`],
       webClientId: `${Config.GOOGLE_CLIENT}`,
     })
   }, [])
@@ -101,7 +101,7 @@ const Login = (props) => {
         }
         setLoading(true)
         const response = await UserAPI.GoogleSignIn(values)
-        console.log(response.data)
+        // console.log(response.data)
         if (response.data.result) {
           await dispatch({
             type: SIGN_IN,
