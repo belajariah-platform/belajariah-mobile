650c5a80d13facfe6c7cccee3360e378fccfdb41
rikijenifer <rikijenifer15@gmail.com>
2022-01-10 11:13:00 +0700

    Update Intens

--- src/navigations/user-navigation/user-profile/user-profile-drawer.navigation.js
+++ src/navigations/user-navigation/user-profile/user-profile-drawer.navigation.js
@@ -130,7 +130,7 @@ const CustomDrawer = ({ navigation }, props) => {
 
           <View style={styles.versionContainer}>
             <Images.LogoBelajariahProfile.default width={24} height={24} />
-            <Text style={styles.version}>Version: v1.4.9-21</Text>
+            <Text style={styles.version}>Version: v1.4.11-21</Text>
           </View>
         </ImageBackground>
       </DrawerContentScrollView>
